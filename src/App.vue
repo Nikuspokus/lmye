4a93df1cdeaea53fbc151b59532b37bd122555ad
NikusM <nicolas.marquillier@gmail.com>
2024-02-24 00:26:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
