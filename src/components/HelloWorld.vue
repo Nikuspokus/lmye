e9d0d0f0eef436e46cad44844768cd52078b67b1
NikusM <nicolas.marquillier@gmail.com>
2024-03-08 15:21:15 +0100

    slider -WIP + galerie - WIP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,50 +14,139 @@ const reset = () => {
 
 <template>
   <div class="test-image red-filter">
-    <h1 class="py-9">{{ msg }}</h1>
-    <div class="card">
-      <div type="button">count is {{ count }}</div>
-      <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test HMR
-      </p>
-      <button
-        class="btn bg-fuchsia-950 hover:bg-fuchsia-700 tototo"
-        @click="count++"
-      >
-        Click me
-      </button>
-
-      <p>
-        Check out
-        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-          >create-vue</a
-        >, the official Vue + Vite starter
-      </p>
-      <p>
-        Install
-        <a href="https://github.com/vuejs/language-tools" target="_blank"
-          >Volar</a
-        >
-        in your IDE for a better DX
-      </p>
-      <button @click="reset">reset</button>
-      <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <!-- <h1 class="py-9">{{ msg }}</h1>
+    <div class="container">
+      <div class="wrapper">
+        <img
+          src="../assets/arno-senoner-oCXVxwTFwqE-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/aromateec--q9tuxLn4hA-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/content-pixie-ZB4eQcNqVUs-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/diana-light-nvQemFKRBUo-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/laura-chouette-CtOA9wbFAdQ-unsplash.jpg"
+          style="width: 500px"
+        />
+      </div>
+    </div> -->
+  </div>
+  <div class="bg-red-400 tutu" style="width: 100vw; height: 100vh">
+    <div class="container">
+      <div class="wrapper">
+        <img
+          src="../assets/arno-senoner-oCXVxwTFwqE-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/aromateec--q9tuxLn4hA-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/content-pixie-ZB4eQcNqVUs-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/diana-light-nvQemFKRBUo-unsplash.jpg"
+          style="width: 500px"
+        />
+        <img
+          src="../assets/laura-chouette-CtOA9wbFAdQ-unsplash.jpg"
+          style="width: 500px"
+        />
+      </div>
     </div>
   </div>
-  <div class="bg-stone-50" style="width: 100vw; height: 100vh"></div>
-  <div class="bg-violet-50" style="width: 100vw; height: 100vh"></div>
-  <div class="bg-stone-50" style="width: 100vw; height: 100vh"></div>
+  <div class="bg-violet-300" style="width: 100vw; height: 100vh">
+    <div class="wrapper2">
+      <div>One</div>
+      <div>Two</div>
+      <div>Three</div>
+      <div>Four</div>
+      <div>Five</div>
+    </div>
+  </div>
+  <div class="bg-stone-300" style="width: 100vw; height: 100vh">
+    <p>Configurateur</p>
+  </div>
+  <div class="bg-amber-300" style="width: 100vw; height: 100vh">
+    <p>footer</p>
+  </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.container {
+  width: 20em;
+  position: relative;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  overflow: hidden;
+}
+.wrapper {
+  display: flex;
+  animation: slide 20s infinite;
+}
+@keyframes slide {
+  0% {
+    transform: translateX(0);
+  }
+  18% {
+    transform: translateX(0%);
+  }
+  20% {
+    transform: translateX(-100%);
+  }
+  38% {
+    transform: translateX(-100%);
+  }
+  40% {
+    transform: translateX(-200%);
+  }
+  58% {
+    transform: translateX(-200%);
+  }
+  60% {
+    transform: translateX(-300%);
+  }
+  78% {
+    transform: translateX(-300%);
+  }
+  80% {
+    transform: translateX(-400%);
+  }
+  98% {
+    transform: translateX(-400%);
+  }
 }
 
 .test-image {
   max-width: 100%;
   height: 100vh;
   background-color: #382961;
+}
+
+.wrapper2 {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 3em;
+  row-gap: 3em;
+}
+
+.wrapper2 > div {
+  border: 2px solid #ffa94d;
+  border-radius: 5px;
+  background-color: #ffd8a8;
+  padding: 8em;
+  color: #d9480f;
 }
 </style>
