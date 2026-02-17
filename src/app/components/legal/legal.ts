97115f7a1d091f0f22aaa35721668b17ff63c562
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2026-02-17 15:18:35 +0100

    fix: mentions legales updated

--- src/app/components/legal/legal.ts
+++ src/app/components/legal/legal.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component({
     selector: 'app-legal',
@@ -9,4 +10,6 @@ import { RouterModule } from '@angular/router';
     templateUrl: './legal.html',
     styleUrls: ['./legal.scss']
 })
-export class LegalComponent { }
+export class LegalComponent {
+    environment = environment;
+}
